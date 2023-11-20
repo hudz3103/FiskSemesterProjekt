ee7e07b42657febd8164b5f4b2cc2d8377561f29
Hudayfa <hhw3103@hotmail.com>
2023-11-20 10:20:58 +0100

    Vis1 branch bliver skabt
    Co-authored-by: TajBaadsgaard <TajBaadsgaard@users.noreply.github.com>

---
+++ Ordensmappe/elephantSQL.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const { Client } = require("pg");
+var pg = require('pg');
+const klient = new Client({
+user: "xlbhzbfg",
+host: "ella.db.elephantsql.com",
+database: "xlbhzbfg", //læg mærke til at user og database er det samme på shared database
+password: "C930ISwG6S2oVNB0JWq5tzqMxGytTJys",
+port: 5432,
+ssl: {
+rejectUnauthorized: false,
+},
+});
+const qry = 'SELECT food_item from food';
+klient.connect();
+console.log("Sker der noget?");
+app.get("/food", async (req, res) => { //FEJL?!?!?!?
+try {
+let queryData = await klient.query(qry);
+res.json({
+"ok": true,
+"foods": queryData.rows, //HER??????
+})
+} catch (error) {
+res.json({
+"ok": false,
+"message": error.message,
+})
+};
+})
+app.listen(port, () => {
+console.log(`Appl. lytter på http://localhost:${port}`);
+});
