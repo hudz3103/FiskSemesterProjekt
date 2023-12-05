0d3a4094bf53312c15806e3f2a19cbb4591c1476
Emma Nøjsen <emmanojsen@gmail.com>
2023-12-05 12:55:15 +0100

    css, js scroll kode  tilføj "reveal" til elementer

--- main.js
+++ main.js
@@ -412,3 +412,21 @@ fetch("http://localhost:3000/plastic")
     // Handle errors during the fetch
     console.error("Fetch error:", error);
   });
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+ 
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+ 
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+ 
+  window.addEventListener("scroll", reveal);
